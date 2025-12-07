ef7eebc66db40b57ab7fa0068aefef5c393fe148
Sherlo07 <Sherlo07>
2025-12-07 18:20:07 +0530

    Updated Projects images 1

--- Models/index.js
+++ Models/index.js
@@ -6,12 +6,11 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) throw new Error("MONGO_URI not defined");
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI not defined");
+    }
 
-    const connection = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const connection = await mongoose.connect(process.env.MONGO_URI);
 
     console.log(`✅ MongoDB Connected: ${connection.connection.host}`);
   } catch (error) {
